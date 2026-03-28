24b9ea4c3bceccd9bccd53c4c3e4c5d8efde98bd
Daniel Orr <orrdaniel73@gmail.com>
2026-03-28 20:24:43 +0000

    feat: duration codec

--- src/main/kotlin/net/mcbrawls/codex/TimeCodecs.kt
+++ src/main/kotlin/net/mcbrawls/codex/TimeCodecs.kt
@@ -1,11 +1,18 @@
 package net.mcbrawls.codex
 
 import com.mojang.serialization.Codec
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Instant
 
 object TimeCodecs {
     val INSTANT: Codec<Instant> = Codec.STRING.xmap(
         Instant::parse,
         Instant::toString,
+    )
+
+    val DURATION: Codec<Duration> = Codec.LONG.xmap(
+        { it.milliseconds },
+        { it.inWholeMilliseconds },
     )
 }
